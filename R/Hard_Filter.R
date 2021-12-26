06f8d65021e79421bbd0cb029376e8564ad74d6b
YuLei <32031495+YULEITSINGTAO@users.noreply.github.com>
2021-12-25 18:09:42 -0800

    Add files via upload

--- R/Hard_Filter.R
+++ R/Hard_Filter.R
@@ -1,43 +1,49 @@
-### The function is used to hard filter the raw VCF_list ###
 
-### This function is used to filter the VCF_List
-
-#' Hard_Filter the VCF_List
+#' Hard Filter the VCF
+#' Hard filter the raw vcf file based on the "PASS", "PRECISE" and "Both"
 #'
 #' @param VCF_List
 #' @param Filter_Standard
 #'
-#' @return
+#' @return Filtered_VCF_List
 #' @export
 #'
 #' @examples
 Hard_Filter <- function(VCF_List, Filter_Standard){
 
     Chromosome <- paste0("chr", c(1:22, "X","Y"))
-
+    Filtered_VCF_List <- list()
 
     if(Filter_Standard == "PASS"){
-        for (i in 1:VCF_list){
-
-
+        for (i in names(VCF_List)){
+            temp_list <- list()
+            for (j in names(VCF_List[[i]])){
+                temp_list[[j]] <- as.data.frame(VCF_List[[i]][[j]]@fix) %>% filter(FILTER =="PASS", CHROM %in% Chromosome)
+            }
+            Filtered_VCF_List[[i]] <- temp_list
         }
-        for (i in 1:length(DELLY_SV_list)){
-            DELLY_VCF_list[[i]] <- as.data.frame(DELLY_SV_list[[i]]@fix)
-        }
-
-
     }
 
     if(Filter_Standard == "Precise"){
-
-
+        for (i in names(VCF_List)){
+            temp_list <- list()
+            for (j in names(VCF_List[[i]])){
+                temp_list[[j]] <- as.data.frame(VCF_List[[i]][[j]]@fix) %>% filter(grepl("PRECISE", INFO, ignore.case = TRUE), CHROM %in% Chromosome)
+            }
+            Filtered_VCF_List[[i]] <- temp_list
+        }
     }
 
     if(Filter_Standard == "Both"){
-
-
+        for (i in names(VCF_List)){
+            temp_list <- list()
+            for (j in names(VCF_List[[i]])){
+                temp_list[[j]] <- as.data.frame(VCF_List[[i]][[j]]@fix) %>% filter(FILTER =="PASS", grepl("PRECISE", INFO, ignore.case = TRUE), CHROM %in% Chromosome)
+            }
+            Filtered_VCF_List[[i]] <- temp_list
+        }
     }
 
-    return ()
+    return(Filtered_VCF_List)
 
 }
