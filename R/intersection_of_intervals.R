1d93f1180fcb7807e5efead4185a1e92705e5ed6
YuLei <32031495+YULEITSINGTAO@users.noreply.github.com>
2023-04-13 18:22:02 -0700

    updates

--- R/intersection_of_intervals.R
+++ R/intersection_of_intervals.R
@@ -1,1 +1,61 @@
-## Intersect of unions
+#' Intersection of intervals
+#'
+#' @param bed_df
+#' @param verbose
+#'
+#' @return
+#' @export
+#'
+#' @examples
+intersection_bed_df <- function(bed_df_1, bed_df_2, verbose = FALSE){
+    intersection_df_list <- list()
+    chromosomes <- paste0("chr", c(1:22,"X","Y"))
+    for (chr in chromosomes) {
+        chr_bed_df_1 <- bed_df_1 %>% dplyr::filter(Chr == chr)
+        chr_bed_df_2 <- bed_df_2 %>% dplyr::filter(Chr == chr)
+
+        if (nrow(chr_bed_df_1)*nrow(chr_bed_df_2)==0){
+            intersection_df_intervals <- data.frame(Chr = NA, Start = NA, End = NA)
+            if (verbose == TRUE){
+                print(paste("There is no SVs in", chr))
+            }
+        }else{
+
+            chr_bed_df_intervals_1 <- intervals::Intervals(chr_bed_df_1 %>% dplyr::select(Start, End))
+            chr_bed_df_intervals_2 <- intervals::Intervals(chr_bed_df_2 %>% dplyr::select(Start, End))
+
+            intersection_df_intervals <- as.data.frame(intervals::interval_intersection(chr_bed_df_intervals_1, chr_bed_df_intervals_2))
+
+            if(nrow(intersection_df_intervals) == 0){
+                if (verbose == TRUE){
+                    print(paste("There is no overlapping SVs in", chr))
+                }
+            }else{
+                intersection_df_intervals <- cbind(chr, intersection_df_intervals)
+                colnames(intersection_df_intervals) <- c("Chr", "Start", "End")
+            }
+        }
+
+        intersection_df_list[[chr]] <- intersection_df_intervals
+
+    }
+    intersection_df <- na.omit(do.call(rbind, intersection_df_list))
+    rownames(intersection_df) <- NULL
+    return(intersection_df)
+
+}
+
+intersection_bed_list <- function(bed_list_1, bed_list_2, verbose = FALSE){
+
+    bed_list <- list()
+
+    for (i in names(bed_list_1)){
+        bed_list[[i]] <- intersection_bed_df(bed_list_1[[i]], bed_list_2[[i]])
+
+    }
+    return(bed_list)
+}
+
+'%I%' <- function(bed_list_1, bed_list_2){
+    intersection_bed_list(bed_list_1, bed_list_2)
+}
--- R/union_of_intervals.R
+++ R/union_of_intervals.R
@@ -7,11 +7,12 @@
 #' @export
 #'
 #' @examples
-union_bed_df <- function(bed_df, verbose = FALSE){
+union_bed_df <- function(bed_df_1, bed_df_2, verbose = FALSE){
     union_df_list <- list()
     chromosomes <- paste0("chr", c(1:22,"X","Y"))
     for (chr in chromosomes) {
-        chr_bed_df <- bed_df %>% dplyr::filter(Chr == chr)
+        chr_bed_df <- rbind(bed_df_1 %>% dplyr::filter(Chr == chr),  bed_df_2 %>% dplyr::filter(Chr == chr))
+
         if (nrow(chr_bed_df)==0){
             union_df_intervals <- data.frame(Chr = NA, Start = NA, End = NA)
             if (verbose == TRUE){
@@ -33,12 +34,17 @@ union_bed_df <- function(bed_df, verbose = FALSE){
 
 }
 
-union_bed_list <- function(bed_list, verbose = FALSE){
-    for (i in names(bed_list)){
-        bed_list[[i]] <- union_bed_df(bed_list[[i]])
+union_bed_list <- function(bed_list_1, bed_list_2, verbose = FALSE){
+
+    bed_list <- list()
+
+    for (i in names(bed_list_1)){
+        bed_list[[i]] <- union_bed_df(bed_list_1[[i]], bed_list_2[[i]])
 
     }
     return(bed_list)
 }
 
-`%U%`= function(x,y){x+y}
+'%U%' <- function(bed_list_1, bed_list_2){
+    union_bed_list(bed_list_1, bed_list_2)
+}
